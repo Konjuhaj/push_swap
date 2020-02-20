54bee4d0e697623fbc58a9f06dcc1d07dcc6c72b
Besnik Konjuhaj <besnik.konjuhaj@gmail.com>
2020-02-20 18:51:58 +0200

    updated script file

--- git_push.sh
+++ git_push.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 read MESSAGE
 git add *
-git commit -m MESSAGE
+git commit -m "$MESSAGE"
 git push
 rm -rf ../GIT_push_swap/*
 cp -rf ../push_swap/* ../GIT_push_swap/
 cd ../GIT_push_swap/
 git add *
-git commit -m MESSAGE
+git commit -m "$MESSAGE"
 git push
