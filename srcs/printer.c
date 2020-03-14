e8b7cc8804e3efc34d4a6557a6997543496ff7c4
Besnik Konjuhaj <besnik.konjuhaj@gmail.com>
2020-03-14 08:17:18 +0200

    Add working printer and error checker

--- srcs/printer.c
+++ srcs/printer.c
@@ -6,11 +6,15 @@
 /*   By: bkonjuha <bkonjuha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 13:43:11 by bkonjuha          #+#    #+#             */
-/*   Updated: 2020/03/14 00:07:48 by bkonjuha         ###   ########.fr       */
+/*   Updated: 2020/03/14 08:15:09 by bkonjuha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+/* TODO
+** printer prints with -e flag. To be fixed
+*/
 
 void	print_dots(int num)
 {
@@ -43,32 +47,38 @@ void	set_color(t_node *curr, t_node *next, int color)
 	}
 }
 
+void	print_a_and_b(t_stack * stack, int i)
+{
+	int data;
+	data = 0;
+	if (i < stack->a_size)
+	{
+		data = stack->flag == 'l' ? stack->a->data : ncount(stack->a->data);
+		set_color(stack->a, stack->a->next, stack->color);
+		stack->flag == 'v' ? print_numbers(stack->a->data) : 0;
+		stack->flag == 'l' ? print_dots(stack->a->data) : 0;
+		stack->a = stack->a->next;
+	}
+	ft_printf("%*c ", (80 - ft_abs(data)), '|');
+	if (i < stack->b_size)
+	{
+		set_color(stack->b, stack->b->next, stack->color);
+		stack->flag == 'v' ? print_numbers(stack->b->data) : 0;
+		stack->flag == 'l' ? print_dots(stack->b->data) : 0;
+		stack->b = stack->b->next;
+	}
+	ft_printf("\n");
+}
+
 void	print_stack(t_stack *stack)
 {
 	int i;
-	int data;
 
 	i = -1;
-	while (++i < stack->a_size || i < stack->b_size)
-	{
-		data = 0;
-		if (i < stack->a_size)
+	if (stack->flag != 'e')
+		while (++i < stack->a_size || i < stack->b_size)
 		{
-			data = stack->flag == 'l' ? stack->a->data : ncount(stack->a->data);
-			set_color(stack->a, stack->a->next, stack->color);
-			stack->flag == 'v' ? print_numbers(stack->a->data) : 0;
-			stack->flag == 'l' ? print_dots(stack->a->data) : 0;
-			stack->a = stack->a->next;
+			print_a_and_b(stack, i);
+			ft_printf("-------\n");
 		}
-		ft_printf("%*c ", (80 - ft_abs(data)), '|');
-		if (i < stack->b_size)
-		{
-			set_color(stack->b, stack->b->next, stack->color);
-			stack->flag == 'v' ? print_numbers(stack->b->data) : 0;
-			stack->flag == 'l' ? print_dots(stack->b->data) : 0;
-			stack->b = stack->b->next;
-		}
-		ft_printf("\n");
-	}
-	ft_printf("-------\n");
 }
